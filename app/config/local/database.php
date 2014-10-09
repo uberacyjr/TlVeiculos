384249a2eea84aa25912ff3b90d433e2d0fc7ca4
uberacyjr <uberacyjr>
2014-10-09 01:26:59 -0300

    teste db
    mais um teste

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'tlveiculos',
+			'username'  => 'forge',
+			'password'  => '4ORSws7AHFfjtSFggV6D',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
