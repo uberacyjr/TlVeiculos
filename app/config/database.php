26c4c27b71e4de7ea76d6045b2270ae6b99d3841
uberacyjr <uberacyjr>
2014-10-09 02:03:05 -0300

    aaaaaaa
    aaaaaaaaaaa

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'tlveiculos',
-			'username'  => 'root',
-			'password'  => '123',
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => '4ORSws7AHFfjtSFggV6D',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
