113b22e40f68d423ce282899385a9aebe375c9f9
uberacyjr <uberacyjr>
2014-10-27 19:11:25 -0200

    mudanças
    go

--- app/views/carros/contatos.blade.php
+++ app/views/carros/contatos.blade.php
@@ -8,9 +8,12 @@
 			    <address><strong>TIM: </strong>  TIM (61) 9294-2671 / (61) 8168-3394 </address>
 			    <address><strong>WHATSAPP: </strong>  (61) 8165-8800  </address>
 			    <address><strong>VIVO: </strong>  (61) 9683-3493  </address>
-			     <address><strong>CLARO: </strong>  (61) 9323-0339  </address>
-			   {{ HTML::image('img/carros-nacionais.png', 'Carros', array( 'style'=>'margin-top:9px')) }}
-			  
+			    <address><strong>CLARO: </strong>  (61) 9323-0339  </address>
+			    <div class="row">
+				    <div class="col-md-4 col-md-offset-4">
+				   		{{ HTML::image('img/carros-nacionais.png', 'Carros', array( 'style'=>'margin-top:9px;', 'class' => 'img-responsive')) }}
+				    </div>
+				</div>
 			</div>
 		
 	</div>
