5bfb32bfa6fe7fa8ac1ef1253d77510ec41d58bd
uberacyjr <uberacyjr>
2014-10-27 15:59:31 -0200

    arrruman do carros
    listando carros

--- app/views/carros/carros.blade.php
+++ app/views/carros/carros.blade.php
@@ -6,7 +6,6 @@
 <div style="height:20px;"></div>
     <!-- Page Content -->
     <div class="container">
-
         <!-- Page Header -->
         <div class="row">
             <div class="col-lg-12">
@@ -19,7 +18,6 @@
         @if(!empty($carros))
             <!-- Projects Row -->
             <div class="row">
-
                 @foreach($carros as $carro)
                     @if( $carro->vendido == 1 or  $carro->vendido == null)
                         <div class="col-md-4 portfolio-item">
@@ -27,20 +25,14 @@
                                 <img class="img-responsive" width="500" src="{{ $carro->pathImagem }}" alt="">
                             </a>
                             <h3>
-                            <p class="text-uppercase">
-                            {{ HTML::linkAction('ConteudoController@show',"$carro->descModelos",array($carro->idImages), array('class' => 'btn btn-info')) }}
-                                
+                                {{ HTML::linkAction('ConteudoController@show',"$carro->descModelos",array($carro->idImages), array('class' => 'btn btn-info')) }}  
                             </h3>
-
-
-
-
                         </div>
                     @endif
-             @endforeach
+                @endforeach
             </div>
             <!-- /.row -->
-         @else
+        @else
             <div  class="col-lg-12"><p>Não possui carro cadastrado</p></div>
         @endif
     </div>
