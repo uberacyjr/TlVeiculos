e5bd756d72b6c3ed70d6b645fa1debdeb87bf697
uberacyjr <uberacyjr>
2014-12-02 10:48:31 -0200

    alteracao tela inicial
    go

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -14,10 +14,7 @@
                         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                             <div class="carousel-inner">
                                  <div class="item active">
-                                    <img class="slide-image" src="img/banner3.png" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="img/slide1.png" alt="">
+                                      <img class="slide-image" src="img/slide1.png" alt="">
                                 </div>
                                 <div class="item ">
                                     <img class="slide-image" src="img/slide2.png" alt="">
@@ -59,7 +56,4 @@
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d350.608206429965!2d-48.03360801263368!3d-15.80309566475932!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTXCsDQ4JzExLjAiUyA0OMKwMDInMDEuOCJX!5e1!3m2!1spt-BR!2sbr!4v1414111871201" width="400" height="300" frameborder="0" style="border:0"></iframe>
               </div>
               <div id="fb-root"></div>
-              
-
-
 @stop
